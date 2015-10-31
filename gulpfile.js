10e4787ac16d79e022236ce20c5fc15569ef14f7
Arun <pratikarun@gmail.com>
2015-10-31 19:42:57 +0530

    Add a clean task. Linked it as a dependency to build.
    Usage of async callbacks for gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require("gulp");
+var rimraf = require("rimraf");
 
 var buildDirectory = "_build";
 
-gulp.task("build", function() {
+gulp.task("build", ["clean"], function() {
     gulp.src("src/app.ts")
         .pipe(gulp.dest(buildDirectory));
+});
+
+gulp.task("clean", function(done) {
+    rimraf(buildDirectory, function() {
+        // rimraf deletes the directory asynchronously
+        done();
+    });
 });
 
 gulp.task("default", ["build"]);
